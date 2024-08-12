4394c9e1c32bacbf8c9324e55d8d6981c69b764f
Arrue42 <paoloarrue27@gmail.com>
2024-08-12 20:39:01 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+
+const GAME_OPTINS ={
+
+    ROCK:'rock',
+    PAPER:'paper',
+    SCISSORS:'scissors'
+}
+
+const playHuman = (humanChoice) => {
+    playTheGame(humanChoice, playMachine())
+
+
+}
+
+const playMachine = () => {
+    const choices = [GAME_OPTINS.ROCK,GAME_OPTINS.PAPER, GAME_OPTINS.SCISSORS]
+    const randomNumber = Math.floor(Math.random() * 3)
+
+
+    return choices[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+    console.log('humano:' + human + " Maquina:" + machine)
+
+    if (human === machine) {
+        result.innerHTML = "Deu empate!"
+
+    } else if ((human === GAME_OPTINS.PAPER && machine === GAME_OPTINS.ROCK) ||
+        (human === GAME_OPTINS.ROCK && machine === GAME_OPTINS.SCISSORS) ||
+        (human === GAME_OPTINS.SCISSORS && machine === GAME_OPTINS.PAPER)) {
+
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+        result.innerHTML = "Você ganhou!"
+
+    } else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = "Você perdeu para a Alexia!"
+    }
+
+}
